9576507c22d050e5dafa321d5a8eb9ef508aa98a
JinHeon Yoon <37974827+yjinheon@users.noreply.github.com>
2022-01-02 00:07:31 +0900

    Create rss_blog.py

---
+++ .github/rss_blog.py
@@ -0,0 +1,26 @@
+import feedparser
+import re
+from pathlib import Path
+
+temp = feedparser.parse('https://yjinheon.github.io/rss2.xml')
+
+def update_readme(readme_base):
+    entries = feedparser.parse('https://yjinheon.github.io/rss2.xml')['entries'][:5]
+    posts = []
+
+    for entry in entries:
+        posts.append(f" - [{entry['title']}]({entry['link']})")
+    posts_joined = '\n'.join(posts)
+
+    return readme_base[:readme_base.find(rss_title)] + f"\n{posts_joined}"
+
+
+#print(fetch_writing())
+
+
+rss_title = "## My Recent Posts"
+readme = Path('../README.md').read_text()
+updated_readme = update_readme(readme)
+
+with open('../README.md') as f:
+    f.writable(updated_readme)
