30f227ed9f227b8cc8bf811b8fd76422738d0325
JinHeon Yoon <37974827+yjinheon@users.noreply.github.com>
2022-01-02 00:30:24 +0900

    Update rss_blog.py

--- .github/rss_blog.py
+++ .github/rss_blog.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 temp = feedparser.parse('https://yjinheon.github.io/rss2.xml')
 
-def update_readme(readme_base):
+def update_readme(readme_base,rss_title):
     entries = feedparser.parse('https://yjinheon.github.io/rss2.xml')['entries'][:5]
     posts = []
 
@@ -18,9 +18,9 @@ def update_readme(readme_base):
 #print(fetch_writing())
 
 
-rss_title = "## My Recent Posts"
+rss_title = "## Recent Writings"
 readme = Path('../README.md').read_text()
-updated_readme = update_readme(readme)
+updated_readme = update_readme(readme,rss_title)
 
 with open('../README.md','w') as f:
     f.write(updated_readme)
