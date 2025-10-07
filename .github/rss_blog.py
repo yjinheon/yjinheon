8e65caaeb39317308c0e8b2fd7999dd0de567d83
jinheonyoon <yjinheon@gmail.com>
2025-10-08 06:58:12 +0900

    feat: parse date by updated date

--- .github/rss_blog.py
+++ .github/rss_blog.py
@@ -18,10 +18,19 @@ def update_readme(readme_base, rss_title):
     # Format the datetime object as YYYY-MM-DD
 
     for entry in entries:
-        parsed_date = datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %Z")
+        if entry["updated"]:
+            parsed_date = datetime.striptime(
+                entry["updated"], "%a, %d %b %Y %H:%M:%S %Z"
+            )
+        else:
+            parsed_date = datetime.striptime(
+                entry["published"], "%a, %d %b %Y %H:%M:%S %Z"
+            )
+
         formatted_date = parsed_date.strftime("%Y-%m-%d")
         posts.append(
-            f" - [{entry['title']}]({entry['links'][0]['href']}) ({formatted_date})"
+            f" - [{entry['title']}]({entry['links'][0]
+                                     ['href']}) ({formatted_date})"
         )
 
     posts_joined = "\n".join(posts)
@@ -39,7 +48,8 @@ def update_readme(readme_base, rss_title):
 rss_title = "## Recent Writings"
 readme = Path("../README.md").read_text()
 updated_readme = update_readme(readme, rss_title)
-updated_time = f"\n Last Updated: {datetime.now(pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')}"
+updated_time = f"\n Last Updated: {datetime.now(
+    pytz.timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')}"
 
 with open("../README.md", "w") as f:
     f.write(updated_readme)
